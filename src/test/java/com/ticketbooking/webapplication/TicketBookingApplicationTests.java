05159cfa77aed4b9e9a71be9295d47f8ace1b999
kajamal <kajasharif9894@gmail.com>
2023-10-18 17:40:54 +0530

    first commit

---
+++ src/test/java/com/ticketbooking/webapplication/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketbooking.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
